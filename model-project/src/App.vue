6a1539825982539ec4b97804dc1af7597be4a09b
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-03 00:39:13 +0600

    start model_project

---
+++ model-project/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <h1>{{title}}</h1>
+  <Modal/>
+</template>
+
+<script>
+import Modal from './components/Model.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: "my 1st app"
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: 2px solid black;
+}
+</style>
---
+++ model-project/src/components/Model.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="dropout">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: rgb(50, 134, 113);
+    border-radius: 10px;
+}
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(155, 67, 67, 0.5);
+    width: 100%;
+    height: 100%;
+}
+</style>
