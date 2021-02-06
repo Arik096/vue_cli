0874f471dd134d593b1c602157b179cb220c8480
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-06 19:00:53 +0600

    Update Model.vue

--- model-project/src/components/Model.vue
+++ model-project/src/components/Model.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="dropout">
-        <div class="modal">
+        <div class="modal" :class="{sale: theme === 'sale', dark: theme === 'dark'}">
+            <h1>modal</h1>
+            <h1>{{header}}</h1>
             <p>modal content</p>
+            <strong>{{text}}</strong>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: ["header", "text" , "theme"],
+}
+</script>
 
 <style>
 .modal{
@@ -20,5 +29,19 @@
     background: rgba(155, 67, 67, 0.5);
     width: 100%;
     height: 100%;
+}
+.modal h1{
+    color:aliceblue;   
+}
+.modal p{
+    font-style: normal;
+}
+.modal.sale{
+    background-color: crimson;
+    color: white;
+}
+.modal.dark{
+    background-color: darkslateblue;
+    color: wheat;
 }
 </style>
