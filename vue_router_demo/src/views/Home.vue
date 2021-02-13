1e8de192bd51625c0293859844afd28b5a48f7c5
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-13 20:38:03 +0600

    Update Home.vue

--- vue_router_demo/src/views/Home.vue
+++ vue_router_demo/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis sapiente ab quos fugit dolore labore dolorem et, doloremque reiciendis ea veniam ad quidem optio, sunt tenetur blanditiis amet nisi animi reprehenderit? Nihil alias hic, voluptatibus ipsa, id voluptate suscipit saepe ipsum veritatis laborum placeat similique asperiores! Error non enim sequi porro, quis doloribus nemo possimus eligendi quaerat officiis totam veritatis tempore autem suscipit deserunt obcaecati quos officia dolorem temporibus nulla laboriosam sit omnis, animi iure? Blanditiis similique molestias ut accusantium corporis dicta, tenetur explicabo sunt, corrupti atque aspernatur eveniet quam perspiciatis nobis, itaque voluptatem enim ab inventore temporibus minima incidunt?</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis sapiente ab quos fugit dolore labore dolorem et, doloremque reiciendis ea veniam ad quidem optio, sunt tenetur blanditiis amet nisi animi reprehenderit? Nihil alias hic, voluptatibus ipsa, id voluptate suscipit saepe ipsum veritatis laborum placeat similique asperiores! Error non enim sequi porro, quis doloribus nemo possimus eligendi quaerat officiis totam veritatis tempore autem suscipit deserunt obcaecati quos officia dolorem temporibus nulla laboriosam sit omnis, animi iure? Blanditiis similique molestias ut accusantium corporis dicta, tenetur explicabo sunt, corrupti atque aspernatur eveniet quam perspiciatis nobis, itaque voluptatem enim ab inventore temporibus minima incidunt?</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis sapiente ab quos fugit dolore labore dolorem et, doloremque reiciendis ea veniam ad quidem optio, sunt tenetur blanditiis amet nisi animi reprehenderit? Nihil alias hic, voluptatibus ipsa, id voluptate suscipit saepe ipsum veritatis laborum placeat similique asperiores! Error non enim sequi porro, quis doloribus nemo possimus eligendi quaerat officiis totam veritatis tempore autem suscipit deserunt obcaecati quos officia dolorem temporibus nulla laboriosam sit omnis, animi iure? Blanditiis similique molestias ut accusantium corporis dicta, tenetur explicabo sunt, corrupti atque aspernatur eveniet quam perspiciatis nobis, itaque voluptatem enim ab inventore temporibus minima incidunt?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
