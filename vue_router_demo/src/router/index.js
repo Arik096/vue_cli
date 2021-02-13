76dc765c68d33230c053f0ce61749904ee83973e
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-13 20:59:32 +0600

    job details created

--- vue_router_demo/src/router/index.js
+++ vue_router_demo/src/router/index.js
@@ -1,25 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
-import Jobs from '../views/Jobs.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobsDetails from '../views/jobs/JobsDetails.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/jobs',
-    name: 'Jobs',
-    component: Jobs
-  }
-]
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobsDetails",
+    component: JobsDetails,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ vue_router_demo/src/views/jobs/JobsDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>Job Details Page</h3>
+  <p><strong>{{ id }}</strong></p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id: this.$route.params.id,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
