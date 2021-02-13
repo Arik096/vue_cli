c93c72fbdb3df8813f15552798b00c347e157a62
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-13 20:37:58 +0600

    Create Jobs.vue

---
+++ vue_router_demo/src/views/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+      <h3>{{job.title}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs: [
+                {title: 'ux', id: 1, slug: 'lorem'},
+                {title: 'web', id: 2, slug: 'lorem'},
+                {title: 'android', id: 3, slug: 'lorem'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
