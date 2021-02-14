a7909c931721bc90cb39432e0a8c36ebfa841069
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-14 23:23:23 +0600

    setup() api started

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>hello world</h1>
+    <p>my name is {{name}}, my age is {{age}}</p>
+    <button @click="handleclick">click me</button>
   </div>
 </template>
 
@@ -10,12 +12,21 @@ export default {
   name: 'Home',
   setup(){
     console.log("setup");
+
+    let name = 'mario';
+    let age = 20;
+
+    const handleclick = () => {
+      console.log("clicked");
+    }
+
+    return{ name, age, handleclick};
   },
   created(){
-    console.log("created");
+    //console.log("created");
   },
   mounted(){
-    console.log("mounted");
+    //console.log("mounted");
   }
 }
 </script>
