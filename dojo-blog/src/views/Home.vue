0e9fdb027c1423805d250d5b2eafb725202e430b
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-15 22:11:50 +0600

    #ref done

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="home">
     <h1>hello world</h1>
-    <p>my name is {{name}}, my age is {{age}}</p>
+    <p ref="p">my name is {{name}}, my age is {{age}}</p>
     <button @click="handleclick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 
 export default {
   name: 'Home',
   setup(){
-    console.log("setup");
+
+    const p = ref(null);
 
     let name = 'mario';
     let age = 20;
 
     const handleclick = () => {
-      console.log("clicked");
+      console.log(p.value);
+      p.value.classList.add('test');
     }
 
-    return{ name, age, handleclick};
+    return{ name, age, handleclick, p};
   },
-  created(){
-    //console.log("created");
-  },
-  mounted(){
-    //console.log("mounted");
-  }
+  created(){},
+  mounted(){}
 }
 </script>
