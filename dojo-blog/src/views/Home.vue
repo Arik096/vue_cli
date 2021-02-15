5f324c38c95352a457d25a883b082c5ae017ebe9
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-15 23:43:05 +0600

    watch and watch effect

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -6,11 +6,12 @@
     <div v-for="name in matchName" :key="name">
       {{name}}
     </div>
+    <button @click="handlewatch">stop watch</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 
 export default {
@@ -20,11 +21,24 @@ export default {
     const Search = ref('');
     const names = ref(['arik','puja','rafi','pavel']);
 
+    const stopwatch = watch( Search, () => {
+      console.log("run");
+    } );
+
+    const stopEffectwatch = watchEffect(  () => {
+      console.log("done", Search.value)
+    });
+
     const matchName = computed( () => {
       return names.value.filter( (name) => name.includes(Search.value));
     });
 
-    return{ names, Search, matchName };
+    const handlewatch = () => {
+      stopwatch();
+      stopEffectwatch();
+    }
+
+    return{ names, Search, matchName, handlewatch };
   },
 }
 </script>
