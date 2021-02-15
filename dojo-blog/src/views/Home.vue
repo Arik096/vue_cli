a92757567c332edeb08e34495b012d38b1f211aa
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-15 23:26:23 +0600

    computed property in compostionn API

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <input type="text" v-model="Search">
+    <p>Searched Term - {{Search}}</p>
+    <div v-for="name in matchName" :key="name">
+      {{name}}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { computed, ref } from 'vue';
+
 
 export default {
   name: 'Home',
   setup(){
 
-    return{};
+    const Search = ref('');
+    const names = ref(['arik','puja','rafi','pavel']);
+
+    const matchName = computed( () => {
+      return names.value.filter( (name) => name.includes(Search.value));
+    });
+
+    return{ names, Search, matchName };
   },
 }
 </script>
